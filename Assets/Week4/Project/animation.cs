05f6c33479f939e628c740da6c655ab4b0671ba7
Keistie <mh5448@nyu.edu>
2021-11-01 19:44:14 -0400

    midterm animation update

--- Assets/Week4/Project/animation.cs
+++ Assets/Week4/Project/animation.cs
@@ -7,20 +7,20 @@ public class animation : MonoBehaviour
      public Animator animator;
 
     void CharacterInput(){
-        if(Input.GetKey(KeyCode.A)){
+        if(!Input.anyKey){
+            animator.SetTrigger("IDLE");
+        }
+        else if(Input.GetKey(KeyCode.A)){
             animator.SetTrigger("LEFT");
         }
-        if(Input.GetKey(KeyCode.D)){
+        else if(Input.GetKey(KeyCode.D)){
             animator.SetTrigger("RIGHT");
         }
     }
-    // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
         CharacterInput();
